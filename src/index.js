813b716e1799b7ea3cb8272a0f1e02c6e731e30b
Denny29 <43240668+Denny29@users.noreply.github.com>
2021-03-20 12:01:52 -0400

    Almost done The else statement doesn't run for even when the if statement is false.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import "./styles.css";
 
 let userInput;
-let result;
+let result = "";
+let words;
+let firstLetter;
 
 $("button").click(function () {
   userInput = $(".input").val();
-  console.log(userInput);
+  words = userInput.split(" ");
+  // console.log(words);
+  for (let word of words) {
+    if (word[0] === "a" || "e" || "i" || "o" || "u") {
+      add(word);
+    } else {
+      firstLetter = word.slice(0, 1);
+      word = word.slice(1);
+      word = word + firstLetter;
+      add(word);
+    }
+  }
+  $(".output").html(`<h3> ${result} </h3>`);
 });
 
+function add(theWord) {
+  theWord = theWord + "ay";
+  result = result + theWord + " ";
+}
+
 // RULES FOR PIGLATIN
-// 1. If the word doesn't begin with a vowel:
+// 1. If the word doesn't begin with a vowel (a, e, i, o, u):
 //  Move all the letters before the first vowel
 //  to the end of the word.
 //  Add "-ay" to the end of the word.
